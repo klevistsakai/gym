ee92822ca9d6caef4714d82d7229afbae5794530
klevis tsakai <tsakai.kevi@gmail.com>
2023-11-14 22:52:10 +0200

    the start of the gym_frontend with react

---
+++ gym_frontend/gym/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard"
+ 
+  
+function App() {
+  return (
+    <Router>
+      <Routes>
+          <Route exact path="/"  element={<Login/>} />
+          <Route path="/register"  element={<Register/>} />
+          <Route path="/dashboard"  element={<Dashboard/>} />
+      </Routes>
+    </Router>
+  );
+}
+  
+export default App;
